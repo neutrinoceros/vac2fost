ff91f359b97c9e1f8dc4eda4d07cb4aac433dd8b
Clément Robert <clement.robert@oca.eu>
2018-12-21 19:19:25 +0100

    fix a bug in pylint test

--- tests/test_code_quality.py
+++ tests/test_code_quality.py
@@ -4,11 +4,11 @@ import pathlib
 testloc = pathlib.Path(__file__).parent.resolve()
 output_file = testloc/"output/pylint.log" 
 mainfile = (testloc/'../vac2fost/vac2fost.py').resolve()
-
-subprocess.check_call(f"pylint {mainfile} > {output_file}", shell=True)
-
 MINIMAL_MARK = 9.70
+
+
 def test_style_standard():
+    subprocess.call(f"pylint {mainfile} > {output_file}", shell=True)
     with open(output_file, mode='r') as log:
         lines = log.readlines()
     sumup_line = lines[-2]
