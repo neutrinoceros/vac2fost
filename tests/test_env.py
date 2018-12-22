5c802aef169d3fa32f9dc037e6b866755de8c47c
Clément Robert <clement.robert@oca.eu>
2018-12-22 12:25:15 +0100

    add tests and quick config

---
+++ tests/test_env.py
@@ -0,0 +1,26 @@
+# devnote : can't pickle an interfarce object because it contains vtk objects...
+import pathlib
+import vac2fost
+from vac2fost import main as app
+import pytest
+
+testdir = pathlib.Path(__file__).absolute().parent
+output_dir = testdir/'output/test_env'
+
+
+@pytest.mark.skipif(not vac2fost.vac2fost.colorama,
+                    reason="Can't test color printing withou colorama")
+def test_with_colorama():
+    itf = app(
+        str(testdir/'sample/vac2fost_conf_quick.nml'),
+        output_dir=output_dir
+    )
+    itf.print_all()
+
+def test_without_colorama():
+    vac2fost.vac2fost.colorama = None
+    itf = app(
+        str(testdir/'sample/vac2fost_conf_quick.nml'),
+        output_dir=output_dir
+    )
+    itf.print_all()
