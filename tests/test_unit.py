321fc898c81cd8b8a4d855c1bdcdd45065ad36a4
Clément Robert <clement.robert@oca.eu>
2019-01-03 17:51:02 +0100

    add assert statement to extrapolation function

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -6,12 +6,12 @@ from astropy.io import fits as pyfits
 from vac2fost.vac2fost import twoD2threeD
 test_dir = pathlib.Path(__file__).resolve().parent
 
-def test_3D_conversion():
-    NR, NPHI, NZ = 3, 5, 7
-    rvect = np.linspace(1, 10, NR)
-    phivect = np.linspace(0, 2*np.pi, NPHI)
-    zvect = np.linspace(0, 1, NZ)
+NR, NPHI, NZ = 3, 5, 7
+rvect = np.linspace(1, 10, NR)
+phivect = np.linspace(0, 2*np.pi, NPHI)
+zvect = np.linspace(0, 1, NZ)
 
+def test_3D_conversion_cst_scale_height():
     #radial powerlaw
     rgrid, phigrid = np.meshgrid(rvect, phivect)
     input_data = rgrid**-0.5
@@ -22,3 +22,16 @@ def test_3D_conversion():
     ##regold with
     #with open(test_dir/"ref/2D-3D.p", mode="wb") as file:
     #    pickle.dump(output_data, file)
+
+
+# def test_3D_conversion_cst_scale_height():
+#     #radial powerlaw
+#     rgrid, phigrid = np.meshgrid(rvect, phivect)
+#     input_data = rgrid**-0.5
+
+#     h = np.
+#     output_data = twoD2threeD(arr2d=input_data, scale_height=h, zvect=zvect)
+#     #ref = pickle.load(open(test_dir/"ref/2D-3D_h.p", mode="rb"))
+#     #assert np.all(output_data == ref)
+#     with open(test_dir/"ref/2D-3D_h.p", mode="wb") as file:
+#         pickle.dump(output_data, file)
