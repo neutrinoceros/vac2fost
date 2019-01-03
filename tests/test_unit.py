d89a9908ca6db270f1e81d6ee2738a39dc85af9c
Clément Robert <clement.robert@oca.eu>
2019-01-03 18:00:46 +0100

    add test on large grids

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -23,7 +23,6 @@ def test_3D_conversion_cst_scale_height():
     #with open(test_dir/"ref/2D-3D.p", mode="wb") as file:
     #    pickle.dump(output_data, file)
 
-
 def test_3D_conversion_var_scale_height():
     #radial powerlaw
     input_data = rgrid**-0.5
@@ -33,4 +32,20 @@ def test_3D_conversion_var_scale_height():
     assert np.all(output_data == ref)
     #regold with
     #with open(test_dir/"ref/2D-3D_h.p", mode="wb") as file:
+    #    pickle.dump(output_data, file)
+
+def test_3D_conversion_large_grid():
+    NR, NPHI, NZ = 23, 61, 7
+    rvect = np.linspace(1, 10, NR)
+    phivect = np.linspace(0, 2*np.pi, NPHI)
+    zvect = np.linspace(0, 1, NZ)
+    rgrid, phigrid = np.meshgrid(rvect, phivect)
+
+    #radial powerlaw
+    input_data = rgrid**-0.5
+    output_data = twoD2threeD(arr2d=input_data, scale_height=0.01, zvect=zvect)
+    ref = pickle.load(open(test_dir/"ref/2D-3D_large.p", mode="rb"))
+    assert np.all(output_data == ref)
+    ##regold with
+    #with open(test_dir/"ref/2D-3D_large.p", mode="wb") as file:
     #    pickle.dump(output_data, file)
