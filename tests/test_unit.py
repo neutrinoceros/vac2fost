f9fe906c21673e07c1ccdf9d2c6519caaf6d4dd9
Clément Robert <clement.robert@oca.eu>
2019-01-04 16:43:34 +0100

    fix typos in test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -72,13 +72,14 @@ def gen_mcfost_grid(output_dir):
     """
     itf = Interface(config_file=test_dir/'sample/vac2fost_conf.nml', output_dir=output_dir, dbg=True)
     itf.output_grid()
-    return index
+    return 1
+
 
 @pytest.mark.skipif(mp.cpu_count()==1, reason="parallel computation only with Ncpus>=2")
 def test_parallel_instanciation():
     """Check that instanciating multiple Interface class object at same
     time and location doesn't create collisions"""
-    output_dir = test_dir/"output/test_parallel_instancitaion/"
+    output_dir = test_dir/"output/test_parallel_instanciation/"
     ncpu = min(mp.cpu_count(), 4)
     with mp.Pool(ncpu) as pool:
         pool.map(gen_mcfost_grid, [output_dir/str(i) for i in range(ncpu)])
