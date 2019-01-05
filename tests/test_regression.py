d2bfec256fb308d7820fca04d8d938f3a843da69
Clément Robert <clement.robert@oca.eu>
2019-01-05 14:19:16 +0100

    add non-axisym reg test

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -6,20 +6,17 @@ from astropy.io import fits as pyfits
 
 from vac2fost import main as app
 
-here = pathlib.Path(__file__).absolute().parent
-outdir = here / 'output/test_regression'
+test_dir = pathlib.Path(__file__).absolute().parent
+outdir = test_dir / 'output/test_regression'
 gridfile = outdir / 'mcfost_grid.fits.gz'
 if gridfile.exists():
     os.remove(gridfile)
 
-itf = app(
-    str(here/'sample/vac2fost_conf.nml'),
-    output_dir=outdir
-)
+itf = app(test_dir/'sample/vac2fost_conf.nml', output_dir=outdir)
 
 class TestRegression:
     def test_mcfost_conf(self):
-        with open(here/'ref/mcfost_conf.para') as fi:
+        with open(test_dir/'ref/mcfost_conf.para') as fi:
             ref_lines = fi.readlines()
         with open(outdir/'mcfost_conf.para') as fi:
             new_lines = fi.readlines()
@@ -27,7 +24,7 @@ class TestRegression:
             assert n == r
 
     def test_target_grid(self):
-        ref = pyfits.open(here/'ref/mcfost_grid.fits.gz')[0].data
+        ref = pyfits.open(test_dir/'ref/mcfost_grid.fits.gz')[0].data
         new = pyfits.open(outdir/'mcfost_grid.fits.gz')[0].data
         np.testing.assert_array_equal(ref, new)
 
@@ -38,11 +35,11 @@ class TestRegression:
         fipath = itf.io['out'].filepath
         data = pyfits.open(fipath)[0].data[0]
 
-        ref = pyfits.open(here/'ref/hd142527_dusty0000.fits')[0].data[0]
+        ref = pyfits.open(test_dir/'ref/hd142527_dusty0000.fits')[0].data[0]
         np.testing.assert_allclose(data, ref, rtol=1e-15)
 
     def test_out(self):
-        out_ref = pickle.load(open(here/'ref/main_out.p', 'rb'))
+        out_ref = pickle.load(open(test_dir/'ref/main_out.p', 'rb'))
         assert itf._dbm == out_ref['_dbm']
         assert itf.sim_conf == out_ref['sim_conf']
         np.testing.assert_array_equal(itf.input_grid['rv'], out_ref['input_grid']['rv'])
@@ -55,7 +52,7 @@ class TestRegression:
         np.testing.assert_allclose(itf.new_3D_arrays, out_ref['new_3D_arrays'], rtol=1e-15)
 
         ##use this to regold the reference file
-        # with open(here/'ref/main_out2.p', 'wb') as file:
+        # with open(test_dir/'ref/main_out2.p', 'wb') as file:
         #     save_keys = ['sim_conf',
         #                  'input_grid', 'output_grid',
         #                  'new_2D_arrays', 'new_3D_arrays',
@@ -63,3 +60,28 @@ class TestRegression:
         #     ]
         #    out = {k: itf.__getattribute__(k) for k in save_keys}
         #    pickle.dump(out, file)
+
+    def test_out_non_axisym(self):
+        outdir = test_dir / 'output/test_regression_non_axisym'
+        itf = app(test_dir/'sample/vac2fost_conf.nml', output_dir=outdir)
+        out_ref = pickle.load(open(test_dir/'ref/main_out_non_axisym.p', 'rb'))
+        assert itf._dbm == out_ref['_dbm']
+        assert itf.sim_conf == out_ref['sim_conf']
+        np.testing.assert_array_equal(itf.input_grid['rv'], out_ref['input_grid']['rv'])
+        np.testing.assert_array_equal(itf.input_grid['phiv'], out_ref['input_grid']['phiv'])
+        np.testing.assert_array_equal(itf.output_grid['rv'], out_ref['output_grid']['rv'])
+        np.testing.assert_array_equal(itf.output_grid['phiv'], out_ref['output_grid']['phiv'])
+        np.testing.assert_array_equal(itf.output_grid['rg'], out_ref['output_grid']['rg'])
+        np.testing.assert_array_equal(itf.output_grid['phig'], out_ref['output_grid']['phig'])
+        np.testing.assert_allclose(itf.new_2D_arrays, out_ref['new_2D_arrays'], rtol=1e-25)
+        np.testing.assert_allclose(itf.new_3D_arrays, out_ref['new_3D_arrays'], rtol=1e-15)
+
+        #use this to regold the reference file
+        # with open(test_dir/'ref/main_out_non_axisym.p', 'wb') as file:
+        #     save_keys = ['sim_conf',
+        #                  'input_grid', 'output_grid',
+        #                  'new_2D_arrays', 'new_3D_arrays',
+        #                  '_dbm'
+        #     ]
+        #     out = {k: itf.__getattribute__(k) for k in save_keys}
+        #     pickle.dump(out, file)
