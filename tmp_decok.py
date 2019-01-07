3d3060ed85d3af7cba4b6432744f042bbf841aa3
Clément Robert <cr52@protonmail.com>
2019-01-07 08:45:20 +0100

    works on class

--- tmp_decok.py
+++ tmp_decok.py
@@ -9,9 +9,9 @@ def parameterized(dec):
     return layer
 
 @parameterized
-def mydecok(func, mess):
+def mydecok(func, mess, lenght=50):
     def modfunc(*args, **kwargs):
-        print(mess.ljust(50), end="... ", flush=True)
+        print(mess.ljust(lenght), end="... ", flush=True)
         res = func(*args, **kwargs)
         print("ok")
         return res
@@ -23,13 +23,32 @@ def f():
 @mydecok("function 2")
 def ff(a:int):
     sleep(a)
-@mydecok("function 3")
+@mydecok("function 3", 60)
 def fff(a, b=6):
     sleep(5)
     return a**b
+
+class Bidon:
+    @mydecok("cfunction 1")
+    def f():
+        sleep(5)
+    @mydecok("cfunction 2")
+    def ff(a:int):
+        sleep(a)
+    @mydecok("cfunction 3")
+    def fff(a, b=6):
+        sleep(5)
+        return a**b
+
+
 # "script"
+
 f()
 ff(2)
 res = fff(5)
+assert res == 15625
 
+Bidon.f()
+Bidon.ff(2)
+res = Bidon.fff(5)
 assert res == 15625
