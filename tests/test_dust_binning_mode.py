a91e450e3db0a7a43746c554afbf0612e31874ef
Clément Robert <clement.robert@oca.eu>
2019-02-06 11:26:59 +0100

    update dust binning mode tests

--- tests/test_dust_binning_mode.py
+++ tests/test_dust_binning_mode.py
@@ -17,36 +17,39 @@ def test_gas_only():
     itf = Interface(test_dir/'sample/vac2fost_conf_quick.nml',
                     output_dir=output_dir,
                     dust_bin_mode="gas-only")
-    grain_micron_sizes = itf.grain_micron_sizes
-    assert grain_micron_sizes == [MINGRAINSIZE_µ]
+    assert itf.dust_binning_mode == "gas-only"
+    assert itf.grain_micron_sizes == [MINGRAINSIZE_µ]
 
 def test_dust_only():
     itf = Interface(test_dir/'sample/vac2fost_conf_quick.nml',
                     output_dir=output_dir,
                     dust_bin_mode="dust-only")
-    grain_micron_sizes = itf.grain_micron_sizes
-    print(grain_micron_sizes)
-    np.testing.assert_array_equal(grain_micron_sizes, [1e4, 1e3])
+    assert itf.dust_binning_mode == "dust-only"
+    np.testing.assert_array_equal(itf.grain_micron_sizes, [1e4, 1e3])
 
 def test_mixed():
     itf = Interface(test_dir/'sample/vac2fost_conf_quick.nml',
                     output_dir=output_dir,
                     dust_bin_mode="mixed")
-    grain_micron_sizes = itf.grain_micron_sizes
-    np.testing.assert_array_equal(grain_micron_sizes, [MINGRAINSIZE_µ, 1e4, 1e3])
+    assert itf.dust_binning_mode == "mixed"
+    np.testing.assert_array_equal(itf.grain_micron_sizes, [MINGRAINSIZE_µ, 1e4, 1e3])
+
+def test_unrecognized_dbm():
+    with pytest.raises(KeyError):
+        itf = Interface(test_dir/'sample/vac2fost_conf_quick.nml',
+                        output_dir=output_dir,
+                        dust_bin_mode="not-a-real-dbm-option")
 
 def test_auto_into_mixed():
     itf = Interface(test_dir/'sample/vac2fost_conf_quick.nml',
                     output_dir=output_dir,
                     dust_bin_mode="auto")
-    itf.grain_micron_sizes
     assert itf.dust_binning_mode == "mixed"
 
 def test_auto_into_gas_only():
     itf = Interface(test_dir/'sample/vac2fost_conf_quick_no_dust.nml',
                     output_dir=output_dir,
                     dust_bin_mode="auto")
-    itf.grain_micron_sizes
     assert itf.dust_binning_mode == "gas-only"
 
 # impossible setups should raise KeyError
