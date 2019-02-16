bc2ea1e78e71a1f5f6cb7eb938bb1f81846557a6
Clément Robert <clement.robert@oca.eu>
2019-02-16 17:18:01 +0100

    add test on mcfost version

--- tests/test_env.py
+++ tests/test_env.py
@@ -2,10 +2,23 @@
 import pathlib
 import vac2fost
 from vac2fost import main as app
+from vac2fost.vac2fost import mcfost_major_version, mcfost_minor_version
 import pytest
+import subprocess
 
 testdir = pathlib.Path(__file__).absolute().parent
 output_dir = testdir/'output/test_env'
+
+def test_mcfost_version():
+    """Check consistency with recommended mcfost version"""
+    bout = subprocess.check_output(["mcfost", "-version"])
+    out = "".join(map(chr, bout))
+    version_tag = out.split("\n")[0].split()[-1]
+    x, y, z = map(int, version_tag.split("."))
+    detected_major = float(f"{x}.{y}")
+    assert detected_major >= float(mcfost_major_version)
+    if detected_major > float(mcfost_major_version):
+        assert z >= int(mcfost_minor_version)
 
 
 @pytest.mark.skipif(not vac2fost.vac2fost.colorama,
