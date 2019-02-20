11df67c4cb2c342d137dbdf6eb9498eaeb816dd6
Clément Robert <clement.robert@oca.eu>
2019-02-20 09:36:59 +0100

    make this work on ci

--- tests/test_read_gas_velocity.py
+++ tests/test_read_gas_velocity.py
@@ -1,7 +1,9 @@
-from vac2fost import main as app
+from vac2fost import main as app, __file__ as v2ffile
 from vac2fost.vac2fost import Interface
 import pytest
+from pathlib import Path
 
+root = Path(v2ffile).parent
 testconf = "tests/sample/vac2fost_conf.nml"
 def test2():
     Interface(testconf, read_gas_velocity=True)
@@ -13,4 +15,4 @@ def test1():
 def test3():
     from subprocess import check_call, CalledProcessError
     with pytest.raises(CalledProcessError):
-        check_call(["vac2fost.py", f"{testconf}", "--read_gas_velocity"])
+        check_call([f"{root}/vac2fost.py", f"{testconf}", "--read_gas_velocity"])
