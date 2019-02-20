153fe23f1e3f3042eb56bb55057234764b9119a0
Clément Robert <clement.robert@oca.eu>
2019-02-20 13:30:22 +0100

    complete rewritting + bugfix

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -38,7 +38,7 @@ class TestShellCalling():
             "python",
             f"{root}/vac2fost.py",
             f"{test_dir}/sample/vac2fost_conf.nml",
-            f"-output {output_dir}"
+            f"--output {output_dir}"
         ])
 
     def test_command_line_call_w_number(self):
@@ -49,24 +49,24 @@ class TestShellCalling():
             "python",
             f"{root}/vac2fost.py",
             f"{test_dir}/sample/vac2fost_conf_quick.nml",
-            f"-output {output_dir}",
+            f"--output {output_dir}",
             "-n 2"
         ])
 
     def test_command_line_call_w_number_argonly(self):
-        output_dir = OUT / "test_command_line_call_w_number_2/"
+        output_dir = OUT / "test_command_line_call_w_number_2"
         if output_dir.is_dir():
             shutil.rmtree(output_dir)
         subprocess.check_call([
             "python",
             f"{root}/vac2fost.py",
             f"{test_dir}/sample/vac2fost_conf_quick_no_number.nml",
-            f"-output {output_dir}",
+            f"--output {output_dir}",
             "-n 2"
         ])
 
     def test_command_line_call_w_number_argonly_zero(self):
-        output_dir = OUT / "test_command_line_call_w_number_3/"
+        output_dir = OUT / "test_command_line_call_w_number_3"
         if output_dir.is_dir():
             shutil.rmtree(output_dir)
         subprocess.check_call([
@@ -81,23 +81,22 @@ class TestShellCalling():
         output_dir = OUT / "test_command_line_call_wo_number"
         if output_dir.is_dir():
             shutil.rmtree(output_dir)
-        comm = ' '.join([
-            str(root / 'vac2fost.py'),
-            str(test_dir / 'sample/vac2fost_conf_quick_no_number.nml'),
-            f'-o {output_dir}',
-        ])
-        exitcode = subprocess.call(comm, shell=True)
-        assert exitcode != 0 # Excepted to fail
+        with pytest.raises(subprocess.CalledProcessError):
+            subprocess.check_call([
+                "python",
+                f"{root}/vac2fost.py",
+                f"{test_dir}/sample/vac2fost_conf_quick_no_number.nml",
+                f"--output {output_dir}",
+            ])
 
     def test_command_line_call_w_multiple_numbers(self):
         output_dir = OUT / "test_command_line_w_multiple_numbers"
         if output_dir.is_dir():
             shutil.rmtree(output_dir)
-        comm = " ".join([
-            str(root / "vac2fost.py"),
-            str(test_dir / "sample/vac2fost_conf_quick_no_number.nml"),
-            f"-n 0 1 2",
-            f"-o {output_dir}",
+        subprocess.check_call([
+            "python",
+            f"{root}/vac2fost.py",
+            f"{test_dir}/sample/vac2fost_conf_quick_no_number.nml",
+            f"--output {output_dir}",
+            "-n 0 1 2"
         ])
-        exitcode = subprocess.call(comm, shell=True)
-        assert exitcode == 0
