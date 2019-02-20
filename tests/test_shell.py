ccd96b972148172383c26e4212be9990577ff813
Clément Robert <clement.robert@oca.eu>
2019-02-20 13:27:12 +0100

    partial rewrite of test_shell

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -1,4 +1,4 @@
-'''test the shell interface'''
+"""test the shell interface"""
 
 import os
 import shutil
@@ -23,71 +23,59 @@ def test_genconf():
     output_dir = OUT / "test_genconf"
     if not output_dir.exists(): os.mkdir(output_dir)
     outfile = output_dir / 'template_vac2fost.nml'
-    comm = ' '.join([
-        str(root / 'vac2fost.py'),
-        f'--genconf > {outfile.resolve()}',
-    ])
-    subprocess.call(comm, shell=True)
-    with open(outfile, mode='rt') as file:
+    subprocess.check_call(["python", f"{root}/vac2fost.py", "--genconf", f"> {outfile.resolve()}"])
+    with open(outfile, mode="rt") as file:
         assert f90nml.read(file)
-    os.remove(outfile)
 
 @pytest.mark.incremental #each test is run only if the previous one passed
 class TestShellCalling():
-    '''require that vac2fost.py be accessible through your $PATH env variable'''
 
     def test_command_line_call(self):
-        output_dir = OUT /"test_command_line_call"
+        output_dir = OUT / "test_command_line_call"
         if output_dir.is_dir():
             shutil.rmtree(output_dir)
-        comm = ' '.join([
-            str(root / 'vac2fost.py'),
-            str(test_dir / 'sample/vac2fost_conf.nml'),
-            f'-o {output_dir}'
+        subprocess.check_call([
+            "python",
+            f"{root}/vac2fost.py",
+            f"{test_dir}/sample/vac2fost_conf.nml",
+            f"-output {output_dir}"
         ])
-        exitcode = subprocess.call(comm, shell=True)
-        assert exitcode == 0
 
     def test_command_line_call_w_number(self):
         output_dir = OUT / "test_command_line_call_w_number_1"
         if output_dir.is_dir():
             shutil.rmtree(output_dir)
-
-        comm = ' '.join([
-            str(root / 'vac2fost.py'),
-            str(test_dir / 'sample/vac2fost_conf_quick.nml'),
-            f'-o {output_dir}',
-            '-n 2'
+        subprocess.check_call([
+            "python",
+            f"{root}/vac2fost.py",
+            f"{test_dir}/sample/vac2fost_conf_quick.nml",
+            f"-output {output_dir}",
+            "-n 2"
         ])
-        exitcode = subprocess.call(comm, shell=True)
-        assert exitcode == 0
-
 
     def test_command_line_call_w_number_argonly(self):
         output_dir = OUT / "test_command_line_call_w_number_2/"
         if output_dir.is_dir():
             shutil.rmtree(output_dir)
-        comm = ' '.join([
-            str(root / 'vac2fost.py'),
-            str(test_dir / 'sample/vac2fost_conf_quick_no_number.nml'),
-            f'-o {output_dir}',
-            '-n 2'
+        subprocess.check_call([
+            "python",
+            f"{root}/vac2fost.py",
+            f"{test_dir}/sample/vac2fost_conf_quick_no_number.nml",
+            f"-output {output_dir}",
+            "-n 2"
         ])
-        exitcode = subprocess.call(comm, shell=True)
-        assert exitcode == 0
 
     def test_command_line_call_w_number_argonly_zero(self):
         output_dir = OUT / "test_command_line_call_w_number_3/"
         if output_dir.is_dir():
             shutil.rmtree(output_dir)
-        comm = ' '.join([
-            str(root / 'vac2fost.py'),
-            str(test_dir / 'sample/vac2fost_conf_quick_no_number.nml'),
-            f'-o {output_dir}',
-            '-n 0'
+        subprocess.check_call([
+            "python",
+            f"{root}/vac2fost.py",
+            f"{test_dir}/sample/vac2fost_conf_quick_no_number.nml",
+            f"-output {output_dir}",
+            "-n 0"
         ])
-        exitcode = subprocess.call(comm, shell=True)
-        assert exitcode == 0
 
     def test_command_line_call_wo_number_at_all(self):
         output_dir = OUT / "test_command_line_call_wo_number"
