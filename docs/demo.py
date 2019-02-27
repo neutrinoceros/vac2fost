d7e6a4a5183f72f7da76656a6124b08860f76192
Clément Robert <clement.robert@oca.eu>
2019-02-27 18:43:29 +0100

    rename and fix demo script

--- docs/demo.py
+++ docs/demo.py
@@ -1,7 +1,7 @@
-'''Visualize the .fits data.
+"""A simple visualisation of a .fits output
 
 Grid info is currently transmitted through app() outputs.
-'''
+"""
 
 import pathlib
 
@@ -15,15 +15,15 @@ doc_dir = pathlib.Path(__file__).absolute().parent
 conf = doc_dir.parent/"tests/sample/vac2fost_conf_nonaxisym.nml"
 output_dir = doc_dir.parent/"demo_out"
 
-if __name__=='__main__':
+if __name__ == "__main__":
     itf = app(conf, output_dir=output_dir, verbose=True)
 
     # get the Primary (only image available),
     # and exctract its first 3d array (density field)
-    filepath = itf.io['out'].filepath
+    filepath = itf.io.OUT.filepath
     data = pyfits.open(filepath)[0].data[0]
-    X = (itf.output_grid['rg'] * np.cos(itf.output_grid['phig']))
-    Y = (itf.output_grid['rg'] * np.sin(itf.output_grid['phig']))
+    X = (itf.output_grid["rg"] * np.cos(itf.output_grid["phig"]))
+    Y = (itf.output_grid["rg"] * np.sin(itf.output_grid["phig"]))
 
     nr, nz, nphi = data.shape
     vertical_profile = data[0,:,0]
@@ -33,7 +33,7 @@ if __name__=='__main__':
     fig, axes = plt.subplots(nrows=3, figsize=(10,15))
     axes[0].set_title("mid plane slice (cartesian)")
     axes[0].set_aspect('equal')
-    axes[0].pcolormesh(X, Y, plane_slice, cmap='inferno')
+    axes[0].pcolormesh(X, Y, plane_slice, cmap="inferno")
 
     axes[1].set_title("vertical distribution (gaussian)")
     axes[1].plot(vertical_profile, lw=0, marker="o")
@@ -41,4 +41,6 @@ if __name__=='__main__':
     axes[2].set_title("vertical slice (not to scale)")
     axes[2].imshow(vertical_slice, cmap="inferno", origin="lower")
 
-    fig.savefig(output_dir/'diagnostic_output.png')
+    savefile = output_dir/"demo_output.png")
+    print(f"Saving ouput to {savefile}")
+    fig.savefig(savefile)
