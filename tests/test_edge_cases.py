4dd1909c90464bccfd2340dcddcf7d812e76b805
Clément Robert <clement.robert@oca.eu>
2019-03-11 15:00:24 +0100

    skip the test unless proper mcfost version is being used

--- tests/test_edge_cases.py
+++ tests/test_edge_cases.py
@@ -2,11 +2,15 @@ from os import chdir
 from shutil import rmtree
 from subprocess import run
 from pathlib import Path
+import pytest
 from vac2fost import main as app
+from vac2fost.vac2fost import DETECTED_MCFOST_VERSION
 
 testdir = Path(__file__).parent.resolve()
 OUT = testdir/"output"
 
+
+@pytest.mark.skipif(DETECTED_MCFOST_VERSION < "3.0.35", reason="bug only with mcfost 3.0.35")
 def test_read_gas_vel_gas_only():
     outdir = OUT / "gasvel_gasonly"
     if outdir.exists():
