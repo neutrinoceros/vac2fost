140067291bdd3d4a03a97a7a0b8e38eb13121a8d
Clément Robert <clement.robert@oca.eu>
2019-05-06 14:26:12 +0200

    add test file

---
+++ tests/int/test_settling.py
@@ -0,0 +1,26 @@
+"""Test on grain-size differentiate settling mode"""
+from shutil import rmtree
+from pathlib import Path
+
+from vac2fost import main as app
+
+
+testdir = Path(__file__).parent.parent
+OUT = testdir / "output"
+densfile = "hd142527_dusty0000.fits"
+
+class TestSettling:
+    outdir = testdir / "output/test_settling"
+    if outdir.is_dir():
+        rmtree(outdir)
+    conf_file = testdir/"sample/vac2fost_conf_quick.nml"
+
+    def test_default_settling(self):
+        itf = app(__class__.conf_file, output_dir=__class__.outdir, mcfost_verbose=True)
+        assert not itf.use_settling
+
+    def test_activate_settling(self):
+        itf = app(__class__.conf_file, output_dir=__class__.outdir,
+                  use_settling=True,
+                  mcfost_verbose=True)
+        assert itf.use_settling
