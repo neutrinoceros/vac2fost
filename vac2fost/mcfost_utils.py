4928a923198fdfc59f176ac87771e2a3e31f66b3
Clément Robert <cr52@protonmail.com>
2019-05-08 11:55:12 +0200

    remove forced acceptance in mcfost_utils

--- vac2fost/mcfost_utils.py
+++ vac2fost/mcfost_utils.py
@@ -22,9 +22,8 @@ MINGRAINSIZE_µ = 0.1
 # mcfost detection ==================================================================
 if shutil.which("mcfost") is None:
     raise OSError("could not find mcfost. Please install mcfost before using vac2fost")
-out = run("yes | mcfost -version", shell=True, capture_output=True).stdout #binary
+out = run("mcfost -version", shell=True, capture_output=True).stdout #binary
 out = "".join(map(chr, out))
-
 DETECTED_MCFOST_VERSION = out.split("\n")[0].split()[-1]
 del out
 if DETECTED_MCFOST_VERSION < MIN_MCFOST_VERSION:
