33b93a0205788e73528b802cbd810c3bf0432e90
Clément Robert <cr52@protonmail.com>
2019-11-06 20:32:48 +0100

    throw away deprecated overriding capabilities of command line tool

--- app/v2f.py
+++ app/v2f.py
@@ -29,28 +29,26 @@ from vac2fost.main import main
 
 
 # Definitions =======================================================================
-def generate_conf_template() -> f90nml.Namelist:
-    """Generate a template namelist object with comments instead of default values"""
-    amrvac_list = dict(
-        hydro_data_dir="path/to/output/data/directory",
-        config="relative/to/<hydro_data_dir>/path/to/amrvac/config/file[s]",
-        nums=0
-    )
+amrvac_list = dict(
+    hydro_data_dir="path/to/output/data/directory",
+    config="relative/to/<hydro_data_dir>/path/to/amrvac/config/file[s]",
+    nums=0
+)
+mcfost_list = dict(
+    n_rad=128, n_rad_in=4, n_az=128, nz=10,
+    # aspect ratio is implied by those parameters
+    flaring_exp=1.125,
+    reference_radius=100.0,
+    scale_height=1.0,  # [a.u.], at defined at reference_radius
+)
 
-    mcfost_list = dict(
-        n_rad=128, n_rad_in=4, n_az=128, nz=10,
-        # aspect ratio is implied by those parameters
-        flaring_exp=1.125,
-        reference_radius=100.0,  # [a.u.]
-        scale_height=1.0,  # [a.u.], at defined at reference_radius
-    )
-    sublists = {
-        "amrvac_input": amrvac_list,
-        "units": DEFAULT_UNITS,
-        "mcfost_output": mcfost_list
-    }
-    template = f90nml.Namelist({k: f90nml.Namelist(v) for k, v in sublists.items()})
-    return template
+sublists = {
+    "amrvac_input": amrvac_list,
+    "mcfost_output": mcfost_list,
+    "units": DEFAULT_UNITS,
+    "flags": {}
+}
+template = f90nml.Namelist({k: f90nml.Namelist(v) for k, v in sublists.items()})
 
 
 def print_mcfost_default_conf():
@@ -67,55 +65,16 @@ if __name__ == "__main__":
     # Parse the script arguments
     parser = ArgumentParser(description="Parse arguments for main app")
     parser.add_argument(
-        dest="configuration", type=str,
+        dest="conf_file", type=str,
         nargs="?",
         default=None,
         help="configuration file (namelist) for this script"
     )
     parser.add_argument(
-        "-n", "--nums", dest="nums", type=int,
-        required=False,
-        default=None,
-        nargs="*",
-        help="output number(s) of the target .vtu VAC output file to be converted" #TODO: rewrite this
-    )
-    parser.add_argument(
-        "-df", "--datfmt", type=str,
-        required=False,
-        default="vtu",
-        help="prefered AMRVAC datfile format (must be vtu or dat)"
-    )
-    parser.add_argument(
-        "-o", "--output", dest="output", type=str,
+        "-o", "--output", dest="output_dir", type=str,
         required=False,
         default=".",
         help="select output directory for generated files"
-    )
-    parser.add_argument(
-        "-dbm", "--dustbinmode", dest="dbm", type=str,
-        required=False,
-        default=None,
-        help="prefered bin selection mode [dust-only, gas-only, mixed]"
-    )
-    parser.add_argument(
-        "--read_gas_density",
-        action="store_true",
-        help="pass gas density to mcfost"
-    )
-    parser.add_argument(
-        "--settling",
-        action="store_true",
-        help="Dubrulle-style grain settling"
-    )
-    parser.add_argument(
-        "--read_gas_velocity",
-        action="store_true",
-        help="pass gas velocity to mcfost (keplerian velocity is assumed otherwise)"
-    )
-    parser.add_argument(
-        "--axisymmetry",
-        action="store_true",
-        help="generate a r-z slice"
     )
     parser.add_argument(
         "-v", "--verbose",
@@ -123,17 +82,12 @@ if __name__ == "__main__":
         help="verbose logging"
     )
     parser.add_argument(
-        "--genconf", action="store_true",
-        help="print a default configuration file for vac2fost"
+        "--template", action="store_true",
+        help="print a configuration file template"
     )
     parser.add_argument(
         "--print_mcfost_defaults", action="store_true",
         help="print internal default values for all mcfost parameters as returned by vac2fost"
-    )
-    parser.add_argument(
-        "--cprofile",
-        action="store_true",
-        help="activate code profiling"
     )
     parser.add_argument(
         "--version",
@@ -148,9 +102,9 @@ if __name__ == "__main__":
         sys.exit(0)
 
     # special cases -----------------------------
-    if cargs.genconf:
-        print(generate_conf_template())
-        print(f"%% automatically generated with vac2fost {__version__}\n")
+    if cargs.template:
+        print(template)
+        print(f"! automatically generated with vac2fost {__version__}\n")
         sys.exit(0)
     elif cargs.print_mcfost_defaults:
         print_mcfost_default_conf()
@@ -159,29 +113,8 @@ if __name__ == "__main__":
         parser.print_help(sys.stderr)
         sys.exit(1)
 
-    if cargs.cprofile:
-        import cProfile
-        import pstats
-        import io
-        pr = cProfile.Profile()
-        pr.enable()
     # -------------------------------------------
-    main(
-        config_file=cargs.configuration,
-        nums=cargs.nums,
-        input_data_format=cargs.datfmt,
-        output_dir=cargs.output.strip(),
-        dust_bin_mode=cargs.dbm,
-        read_gas_density=cargs.read_gas_density,
-        read_gas_velocity=cargs.read_gas_velocity,
-        settling=cargs.settling,
-        axisymmetry=cargs.axisymmetry,
-        loglevel={True: 0, False: 30}[cargs.verbose],
+    main(cargs.conf_file,
+         output_dir=cargs.output_dir.strip(),
+         loglevel={True: 0, False: 30}[cargs.verbose],
     )
-    # -------------------------------------------
-    if cargs.cprofile:
-        pr.disable()
-        s = io.StringIO()
-        ps = pstats.Stats(pr, stream=s).sort_stats("cumulative")
-        ps.print_stats()
-        print(s.getvalue())
