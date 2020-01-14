2060b0e8b010841fffb2d6b68447aec8d84e4c0d
Clément Robert <cr52@protonmail.com>
2020-01-14 13:48:33 +0100

    avoid repeating code

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 """Commmon definitions for test files"""
 from pathlib import Path
 
-TEST_DIR = (Path(__file__).parent / "tests").resolve()
+TEST_DIR = (Path(__file__).parent).resolve()
 TEST_ARTIFACTS_DIR = TEST_DIR / ".artifacts"  # replace "output/"
 TEST_DATA_DIR = TEST_DIR / "data"   # replace "sample/"
 TEST_ANSWER_DIR = TEST_DATA_DIR / "answer"  # replace "ref/"
--- tests/test_regression.py
+++ tests/test_regression.py
@@ -31,36 +31,10 @@ def regold(itf, reffile, morekeys: list = None):
         pickle.dump(out, file)
 
 
-class TestRegressionMain:
-    subrefdir = TEST_ANSWER_DIR / "default"
-    v2flogger.setLevel(10)  # debug
-    itf = instanciate_interface(conffile="vac2fost_conf.nml", read_gas_velocity=True)
-    itf.preroll_mcfost()
-    itf.tag = itf.conf_file.stem
-
-    def test_mcfost_conf(self):
-        itf = __class__.itf
-        with open(itf.io.OUT.directory / "mcfost_conf.para") as fi:
-            new_lines = fi.readlines()
-        # with open(__class__.subrefdir / "mcfost_conf.para", mode="wt") as fi: #regold...
-        #    fi.write("".join(new_lines))
-        with open(__class__.subrefdir / "mcfost_conf.para") as fi:
-            ref_lines = fi.readlines()
-        for n, r in zip(new_lines[2:-3], ref_lines[2:]):
-            assert n == r
-
-    def test_target_grid(self):
-        itf = __class__.itf
-        ref_file = __class__.subrefdir / "mcfost_grid.fits.gz"
-        new_file = itf.io.OUT.directory / "mcfost_grid.fits.gz"
-        ref = fits.open(ref_file)[0].data
-        new = fits.open(new_file)[0].data
-        # shutil.copyfile(new_file, ref_file) # to regold ...
-        np.testing.assert_allclose(ref, new, rtol=1e-15)
-
+class AbstractTestRegression:
     def test_out(self):
-        itf = __class__.itf
-        reffile = __class__.subrefdir / f"{itf.tag}.p"
+        itf = self.__class__.itf
+        reffile = self.__class__.subrefdir / f"{itf.tag}.p"
         # regold(itf, reffile)
 
         out_ref = pickle.load(open(reffile, mode="rb"))
@@ -84,21 +58,57 @@ class TestRegressionMain:
             itf.output_grid["phi-slice_z"], out_ref["output_grid"]["phi-slice_z"], rtol=1e-15
         )
         np.testing.assert_allclose(itf.get_output_ndarray(), out_ref["output_ndarray"], rtol=5e-14)
-        np.testing.assert_allclose(
-            itf.get_gas_velocity_ndarray(), out_ref["output_gas_velocity"], rtol=5e-14
-        )
+        if "output_gas_velocity" in out_ref.keys():
+            np.testing.assert_allclose(
+                itf.get_gas_velocity_ndarray(), out_ref["output_gas_velocity"], rtol=5e-14
+            )
 
+class AbstractTestImage(AbstractTestRegression):
     def test_image(self):
-        itf = __class__.itf
+        itf = self.__class__.itf
         # get the Primary (only image available),
         # and exctract its first 3d array (density field)
         itf.write_output()
-        ref_file = __class__.subrefdir / "hd142527_dusty0000.fits"
+        ref_file = self.__class__.subrefdir / "hd142527_dusty0000.fits"
         new_file = itf.io.OUT.filepath
         # shutil.copyfile(new_file, ref_file) # to regold ...
         ref = fits.open(ref_file)[0].data[0]
         new = fits.open(new_file)[0].data[0]
         np.testing.assert_allclose(new, ref, rtol=5e-14)
+
+class AbstractTestConf(AbstractTestRegression):
+    def test_mcfost_conf(self):
+        itf = self.__class__.itf
+        with open(itf.io.OUT.directory / "mcfost_conf.para") as fi:
+            new_lines = fi.readlines()
+        # with open(__class__.subrefdir / "mcfost_conf.para", mode="wt") as fi: #regold...
+        #    fi.write("".join(new_lines))
+        with open(self.__class__.subrefdir / "mcfost_conf.para") as fi:
+            ref_lines = fi.readlines()
+        for n, r in zip(new_lines[2:-3], ref_lines[2:]):
+            assert n == r
+
+# Actual test classes:
+
+class TestRegressionMain(AbstractTestConf, AbstractTestImage):
+    subrefdir = TEST_ANSWER_DIR / "default"
+    v2flogger.setLevel(10)  # debug
+    itf = instanciate_interface(conffile="vac2fost_conf.nml", read_gas_velocity=True)
+    itf.preroll_mcfost()
+    itf.tag = itf.conf_file.stem
+
+class TestRegressionNonAxisym(AbstractTestImage):
+    subrefdir = TEST_ANSWER_DIR / "nonaxisym"
+    itf = instanciate_interface(conffile="vac2fost_conf_nonaxisym.nml", read_gas_velocity=True)
+    itf.preroll_mcfost()
+    itf.tag = itf.conf_file.stem
+
+
+class TestRegressionAutoGasOnly(AbstractTestRegression):
+    subrefdir = TEST_ANSWER_DIR / "autogasonly"
+    itf = instanciate_interface(conffile="autogasonly/rwi.nml")
+    itf.preroll_mcfost()
+    itf.tag = itf.conf_file.stem
 
 
 class TestRegressionMutliNums:
@@ -112,106 +122,12 @@ class TestRegressionMutliNums:
     itf.tag = itf.conf_file.stem + "multinums"
 
     def test_multinums_output(self):
-        filename = __class__.itf.io.OUT.filepath.stem[:-4]
+        filename = self.__class__.itf.io.OUT.filepath.stem[:-4]
         for n in (0, 1, 2):
-            new_file = __class__.itf.io.OUT.directory / f"{filename}{str(n).zfill(4)}.fits"
-            ref_file = __class__.subrefdir / f"hd142527_dusty{str(n).zfill(4)}.fits"
+            new_file = self.__class__.itf.io.OUT.directory / f"{filename}{str(n).zfill(4)}.fits"
+            ref_file = self.__class__.subrefdir / f"hd142527_dusty{str(n).zfill(4)}.fits"
             assert new_file.exists()
             # shutil.copyfile(new_file, ref_file) # to regold ...
             np.testing.assert_allclose(
                 fits.open(new_file)[0].data, fits.open(ref_file)[0].data, rtol=5e-14
             )
-
-
-class TestRegressionNonAxisym:
-    subrefdir = TEST_ANSWER_DIR / "nonaxisym"
-    itf = instanciate_interface(conffile="vac2fost_conf_nonaxisym.nml", read_gas_velocity=True)
-    itf.preroll_mcfost()
-    itf.tag = itf.conf_file.stem
-
-    def test_out(self):
-        itf = __class__.itf
-        reffile = __class__.subrefdir / f"{itf.tag}.p"
-        # regold(itf, reffile)
-
-        out_ref = pickle.load(open(reffile, mode="rb"))
-        assert itf._dust_bin_mode == out_ref["_dust_bin_mode"]
-        assert itf.amrvac_conf == out_ref["amrvac_conf"]
-        np.testing.assert_array_equal(itf.input_grid["ticks_r"], out_ref["input_grid"]["ticks_r"])
-        np.testing.assert_array_equal(
-            itf.input_grid["ticks_phi"], out_ref["input_grid"]["ticks_phi"]
-        )
-        np.testing.assert_array_equal(itf.output_grid["ticks_r"], out_ref["output_grid"]["ticks_r"])
-        np.testing.assert_array_equal(
-            itf.output_grid["ticks_phi"], out_ref["output_grid"]["ticks_phi"]
-        )
-        np.testing.assert_array_equal(
-            itf.output_grid["z-slice_r"], out_ref["output_grid"]["z-slice_r"]
-        )
-        np.testing.assert_array_equal(
-            itf.output_grid["z-slice_phi"], out_ref["output_grid"]["z-slice_phi"]
-        )
-        np.testing.assert_allclose(
-            itf.output_grid["phi-slice_z"], out_ref["output_grid"]["phi-slice_z"], rtol=1e-15
-        )
-        np.testing.assert_allclose(itf.get_output_ndarray(), out_ref["output_ndarray"], rtol=5e-14)
-        np.testing.assert_allclose(
-            itf.get_gas_velocity_ndarray(), out_ref["output_gas_velocity"], rtol=5e-14
-        )
-
-    def test_image(self):
-        itf = __class__.itf
-        # get the Primary (only image available),
-        # and exctract its first 3d array (density field)
-        itf.write_output()
-        ref_file = __class__.subrefdir / "hd142527_rphi0020.fits"
-        new_file = itf.io.OUT.filepath
-        ref = fits.open(ref_file)[0].data[0]
-        new = fits.open(new_file)[0].data[0]
-        # shutil.copyfile(new_file, ref_file) # to regold ...
-        np.testing.assert_allclose(new, ref, rtol=5e-14)
-
-
-class TestRegressionAutoGasOnly:
-    subrefdir = TEST_ANSWER_DIR / "autogasonly"
-    itf = instanciate_interface(conffile="autogasonly/rwi.nml")
-    itf.preroll_mcfost()
-    itf.tag = itf.conf_file.stem
-
-    def test_mcfost_conf(self):
-        itf = __class__.itf
-        with open(itf.io.OUT.directory / "mcfost_conf.para") as fi:
-            new_lines = fi.readlines()
-        # with open(__class__.subrefdir / "mcfost_conf.para", mode="wt") as fi: #regold...
-        #    fi.write("".join(new_lines))
-        with open(__class__.subrefdir / "mcfost_conf.para") as fi:
-            ref_lines = fi.readlines()
-        for n, r in zip(new_lines[2:-3], ref_lines[2:]):
-            assert n == r
-
-    def test_out(self):
-        itf = __class__.itf
-        reffile = __class__.subrefdir / f"{itf.tag}.p"
-        # regold(itf, reffile)
-
-        out_ref = pickle.load(open(reffile, mode="rb"))
-        assert itf._dust_bin_mode == out_ref["_dust_bin_mode"]
-        assert itf.amrvac_conf == out_ref["amrvac_conf"]
-        np.testing.assert_array_equal(itf.input_grid["ticks_r"], out_ref["input_grid"]["ticks_r"])
-        np.testing.assert_array_equal(
-            itf.input_grid["ticks_phi"], out_ref["input_grid"]["ticks_phi"]
-        )
-        np.testing.assert_array_equal(itf.output_grid["ticks_r"], out_ref["output_grid"]["ticks_r"])
-        np.testing.assert_array_equal(
-            itf.output_grid["ticks_phi"], out_ref["output_grid"]["ticks_phi"]
-        )
-        np.testing.assert_array_equal(
-            itf.output_grid["z-slice_r"], out_ref["output_grid"]["z-slice_r"]
-        )
-        np.testing.assert_array_equal(
-            itf.output_grid["z-slice_phi"], out_ref["output_grid"]["z-slice_phi"]
-        )
-        np.testing.assert_array_equal(
-            itf.output_grid["phi-slice_z"], out_ref["output_grid"]["phi-slice_z"]
-        )
-        np.testing.assert_allclose(itf.get_output_ndarray(), out_ref["output_ndarray"], rtol=1e-15)
